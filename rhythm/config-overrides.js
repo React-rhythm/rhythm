b623bc89d60b3bf15b760475a5cf471dd9638050
Sefan <962007314@qq.com>
2020-10-19 16:59:58 +0800

    create-react-app

---
+++ rhythm/config-overrides.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+const { 
+    override, 
+    fixBabelImports,
+    addWebpackResolve,
+    addWebpackAlias
+  } = require('customize-cra')
+  
+module.exports = override(
+   fixBabelImports('import', {
+     libraryName: 'antd-mobile',
+     style: 'css',
+   }),
+   addWebpackResolve({
+     extensions:[".js",".json",".jsx"]
+   }),
+   addWebpackAlias({
+     "@":path.resolve(__dirname,"src/"),
+     "@a":path.resolve(__dirname,"src/assets/"),
+     "@c":path.resolve(__dirname,"src/components/")
+   })
+ );
---
+++ rhythm/src/index.js
@@ -0,0 +1,11 @@
+import RecatDom from "react-dom"
+import React from "react"
+
+import App from "./App"
+
+import "@a/styles/reset.css"
+
+RecatDom.render(
+    <App></App>,
+    document.querySelector("#root")
+)
