eb6abce9ea6df10ec8a3b97332545e8f74b0b8cf
Sefan <962007314@qq.com>
2020-10-20 23:17:58 +0800

    profile news

--- rhythm/src/App.jsx
+++ rhythm/src/App.jsx
@@ -1,12 +1,17 @@
 import React,{Component} from 'react'
-import Swiper from "./home/index/Swiper"
-import Home from "./home/Home"
+// import Swiper from "./home/index/Swiper"
+// import Home from "./home/Home"
+// import Notice from "./components/notice/Notice"
+
+import Profile from "./home/profile/Profile"
 class App extends Component{
     render(){
         return (
             <>
-            <Swiper></Swiper>
-            <Home></Home>
+            {/* <Swiper></Swiper>
+            <Home></Home> */}
+            {/* <Notice></Notice> */}
+            <Profile></Profile>
             </>
             
         )
