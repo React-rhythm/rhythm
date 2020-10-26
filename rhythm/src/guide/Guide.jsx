9ce6495f6052017a97d2ab6a8aff41a9b9ca4282
chengxiangyi <535964113@qq.com>
2020-10-26 08:22:08 +0800

    test

---
+++ rhythm/src/guide/Guide.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Guide extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default Guide;
